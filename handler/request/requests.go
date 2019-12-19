632eee41e30303351bb6be356858fb60ce3c7699
randalrausch <randy@socialtorch.org>
2019-12-19 16:40:52 -0500

    actually declare the var and improve logging

--- handler/request/requests.go
+++ handler/request/requests.go
@@ -101,13 +101,16 @@ func submitRequest(req events.APIGatewayProxyRequest) (events.APIGatewayProxyRes
 		return clientError(http.StatusBadRequest, errors.New("no location included in request"))
 	}
 
+	var response repository.RequestResponse
 	// If this is a new request, initialize a new request.  If this is an existing request, update it
 	if Open311request.ServiceRequestID == "" {
 		// Create new Open311 Request and load into DynamoDB Requests table
-		response, err := repository.SubmitRequest(Open311request, userID)
+		response, err = repository.SubmitRequest(Open311request, userID)
+		infoLogger.Println("New request submitted: " + response.ServiceRequestID)
 	} else {
 		// Update existing Open311 Request in DynamoDB Requests table
-		response, err := repository.UpdateRequest(Open311request, userID)
+		response, err = repository.UpdateRequest(Open311request, userID)
+		infoLogger.Println("Request updated: " + response.ServiceRequestID)
 	}
 
 	if err != nil {
@@ -118,8 +121,6 @@ func submitRequest(req events.APIGatewayProxyRequest) (events.APIGatewayProxyRes
 	if err != nil {
 		return serverError(http.StatusInternalServerError, errors.New("unable to marshal JSON for request response"))
 	}
-
-	infoLogger.Println("New Request submitted: " + response.ServiceRequestID)
 
 	return events.APIGatewayProxyResponse{
 		StatusCode: http.StatusCreated,

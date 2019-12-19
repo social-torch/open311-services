de8ae34d215988812c4878e25a0140abfbfc8efe
Joel Markham <markhamj@mac.com>
2019-12-19 13:23:44 -0500

    Updates for feedback etc.

---
+++ handler/images/images_test.go
@@ -0,0 +1,8 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestStub(t *testing.T) {
+}
---
+++ handler/user/users_test.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestStub(t *testing.T) {
+	// see https://github.com/aws/aws-sdk-go/blob/master/example/service/dynamodb/unitTest/unitTest_test.go
+}
